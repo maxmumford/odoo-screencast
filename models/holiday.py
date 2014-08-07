3743330d9c5e92b172048e87ef08097010cf2e49
Max Mumford <maxmumford@gmail.com>
2014-08-07 09:50:41 +0200

    7) Add maximum_guests field and graph view

--- models/holiday.py
+++ models/holiday.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from openerp.models import Model
-from openerp.fields import Char
-
+from openerp.fields import Char, Integer
 
 class holiday(Model):
     _name = "holidays.holiday"
     _inherit = ['mail.thread']
 
     name = Char()
+    maximum_guests = Integer()
